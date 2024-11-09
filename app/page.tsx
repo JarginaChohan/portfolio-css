3e79cfcc1d40b2d8eac489045d3153163e86e17a
bilalfareed83 <53287592+bilalfareed83@users.noreply.github.com>
2024-11-09 22:42:29 +0500

    init

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import ResumeBuilder from "./component/resume-builder";
+import Portfolio from "./component/resume-builder";
+
+export default function Home() {
+  return (
+    <div>
+      <Portfolio/>
+    </div>
+  );
+}
